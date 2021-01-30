8cd44de1c991fbc95618948d29ed578f64b78fb9
zxd <18532673661@163.com>
2021-01-30 16:51:33 +0800

    Initial commit

---
+++ src/pages/home/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="navbar">
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Navbar",
+    components: {
+
+    },
+    mixins: [],
+    props: {
+
+    },
+    data: function() {
+      return {
+        message: "Hello Navbar"
+      }
+    },
+    created: function() {
+
+    },
+    mounted: function() {
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+  .navbar {
+    
+  }
+</style>
